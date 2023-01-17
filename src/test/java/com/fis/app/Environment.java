f9b2e7b78e20b14c93528909837bac7a77608148
Tomi <ahmadbustomi1507@gmail.com>
2023-01-17 15:24:07 +0700

    delete typo

--- src/test/java/com/fis/app/Environment.java
+++ src/test/java/com/fis/app/Environment.java
@@ -31,7 +31,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Log4j2
 public class Environment {
 	@Autowired
-	protected static RedisTemplate<String, String> redisTemplate;
+	protected RedisTemplate<String, String> redisTemplate;
 	@Autowired
 	protected TestRestTemplate testRestTemplate;
 	@Autowired
